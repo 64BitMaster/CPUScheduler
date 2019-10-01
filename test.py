6d9caffba70c42ab634642fc269bfc6cd261660b
Marc Sustaita <marc.sustaita@gmail.com>
2019-09-30 23:33:29 -0500

    Adjusted the eventHandler()
    Also added the sorted option, setting sort = True at the top lets you try with a sorted queue, setting to false continue with the normal FCFS method

--- test.py
+++ test.py
@@ -6,17 +6,18 @@ from enum import Enum, auto
 StartTime = datetime.datetime.now()
 CurrentTime = StartTime
 elapsedMilliseconds = 0
+millisecondCounter = 0
 counter = 0
 TOTAL_PROCESSES = 10000
 RAND_MAX = 2147483647
+
+sort = True
 
 readyQueue = []
 eventQueue = []
 finishedList = []
 
 currentLambda = 1
-
-
 
 class eventTypes(Enum):
 	CREATE_PROCESSES = auto()
@@ -41,33 +42,42 @@ def scheduleEvent(event):
 	eventQueue.append(event)
 
 def eventHandler():
-
-	currentEvent = eventQueue.pop(0)
-	if currentEvent.eventType == "CREATE_PROCESSES":
-		createProcesses()
-	elif currentEvent.eventType == "MOVE_TO_READY":
-		selectNextProcess()
-		CPUSimulator()
-	elif currentEvent.eventType == "MARK_AS_COMPLETE":
-		markAsComplete()
-	elif currentEvent.eventType == "RETURN_TO_READY":
-		returnToReady()
+	global millisecondCounter
+	if eventQueue:
+		#print("\nEvent Queue is not empty")
+		currentEvent = eventQueue.pop(0)
+		if currentEvent.eventType == "CREATE_PROCESSES":
+			#print("Creating Processes")
+			createProcesses()
+		elif (currentEvent.eventType == "MOVE_TO_READY" and readyQueue):
+			#print("Selecting next process")
+			selectNextProcess()
+			CPUSimulator()
+		elif currentEvent.eventType == "MARK_AS_COMPLETE":
+			#print("Marking process as complete")
+			markAsComplete()
+		elif currentEvent.eventType == "RETURN_TO_READY":
+			#print("fuck4")
+			returnToReady()
+		else:
+			return -1
 	else:
-		return -1
+		millisecondCounter = millisecondCounter + 100
 
 def simulator():
-
+	global millisecondCounter
+	scheduleEvent(event("CREATE_PROCESSES"))
+	
 	while checkForCompletion() == False:
-		scheduleEvent(event("CREATE_PROCESSES"))
-		scheduleEvent(event("MOVE_TO_READY"))
+		#print(str(eventQueue[0].eventType))
+		if millisecondCounter > 1000:
+			scheduleEvent(event("CREATE_PROCESSES"))
+			millisecondCounter = 0
+		
+		if readyQueue:
+			scheduleEvent(event("MOVE_TO_READY"))
 		eventHandler()
 		
-		
-		
-		
-			
-			
-			
 		
 def createProcesses():
 	processGenerator(currentLambda)
@@ -86,10 +96,13 @@ def CPUSimulator():
 	global elapsedMilliseconds
 	global counter
 	global readyQueue
+	global millisecondCounter
 	
 	global currentProcess
-	print("Time Remaining: " + str(currentProcess.timeRemaining))
-	elapsedMilliseconds = elapsedMilliseconds + (currentProcess.timeRemaining/1000)
+	#print("Time Remaining: " + str(currentProcess.timeRemaining))
+	elapsedMilliseconds = elapsedMilliseconds + (currentProcess.timeRemaining)
+	millisecondCounter = millisecondCounter + (currentProcess.timeRemaining)
+	
 	currentProcess.timeRemaining = 0
 	counter = counter + 1
 	
@@ -112,12 +125,23 @@ def genexp(lam):
 	
 	
 def processGenerator(numberOfProcesses):
+	global sort
 	for i in range(0, numberOfProcesses):
 		nextTotalTime = int(1000*genexp(1/0.06))
 		if nextTotalTime == 0:
 			nextTotalTime = 1
-		print(nextTotalTime)
-		readyQueue.append(process(nextTotalTime))
+			
+		if sort == True:
+			if len(readyQueue) == 0:
+				readyQueue.append(process(nextTotalTime))
+			else:
+				for j in range(len(readyQueue)):
+					#print(str(j) + "\n" + str(len(readyQueue)))
+					if readyQueue[j].timeRemaining > nextTotalTime:
+						readyQueue.insert(j, process(nextTotalTime))
+						break
+		else:
+			readyQueue.append(process(nextTotalTime))
 	
 def checkForCompletion():
 		if counter < TOTAL_PROCESSES:
@@ -134,10 +158,37 @@ def sortByTimeRemaning(readyQueue):
 			if readyQueue[j].timeRemaining > readyQueue[j+1].timeRemaining:
 				readyQueue[j].timeRemaining, readyQueue[j+1].timeRemaining = readyQueue[j+1].timeRemaining, readyQueue[j].timeRemaining
 
+def resetSimulator():
+	global eventQueue
+	global readyQueue
+	global elapsedMilliseconds
+	global counter
+	global StartTime
+	global millisecondCounter
+	eventQueue.clear()
+	readyQueue.clear()
+	elapsedMilliseconds = 0
+	millisecondCounter = 0
+	counter = 0
+	StartTime = datetime.datetime.now()
+	
+
+#print(str(int(1000*genexp(1/0.06))) + " milliseconds")
 
 
-print(str(int(1000*genexp(1/0.06))) + " milliseconds")
 
-simulator()
-print(StartTime)
-print(elapsedMilliseconds)
+
+
+
+for i in range(1, 31):
+	
+	currentLambda = i
+	simulator()
+	print("\nCurrent Lambda: " + str(i))
+	print("Start Time: " + str(StartTime))
+	print("Total Milliseconds: " + str(elapsedMilliseconds))
+	print("Final Length of readyQueue: " + str(len(readyQueue)))
+	resetSimulator()
+	
+	
+
