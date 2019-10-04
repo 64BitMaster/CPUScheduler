758b5433b5620350fe5f37b4d984e18636502cc6
Marc Sustaita <marc.sustaita@gmail.com>
2019-10-03 22:44:35 -0500

    Added comments, deleted unused code, and made switching run types easier

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ CPUEmpty = True
 sort = True
 RR = False
 
-currentQuantum = 0.001
+currentQuantum = 0.2
 
 readyQueue = []
 eventQueue = []
@@ -29,6 +29,12 @@ class eventTypes(Enum):
 	MOVE_TO_READY = auto()
 	MARK_AS_COMPLETE = auto()
 	RETURN_TO_READY = auto()
+	
+class runTypes(Enum):
+	FCFS = auto()
+	SRTF = auto()
+	RR02 = auto()
+	RR001 = auto()
 	
 class event():
 	def __init__(self, eventType):
@@ -48,70 +54,97 @@ def printProcesses(readyQueue):
 
 
 
-
-def scheduleEvent(event):
-	eventQueue.append(event)
-
-def eventHandler():
-	global CPUEmpty
-	global millisecondCounter
-	if eventQueue:
-		
-		currentEvent = eventQueue.pop(0)
-		#print("Current Event: " + str(currentEvent.eventType))
-		if currentEvent.eventType == "CREATE_PROCESSES":
-			#print("Creating Processes")
-			createProcesses()
-		elif (currentEvent.eventType == "MOVE_TO_READY" and readyQueue):
-			#print("Selecting next process")
-			selectNextProcess()
-			CPUSimulator()
-			CPUEmpty = False
-		elif currentEvent.eventType == "MARK_AS_COMPLETE":
-			#print("Marking process as complete")
-			markAsComplete()
-			CPUEmpty = True
-		elif currentEvent.eventType == "RETURN_TO_READY":
-			#print("fuck4")
-			returnToReady()
-			CPUEmpty = True
-		else:
-			return -1
-	else:
-		millisecondCounter = millisecondCounter + 1
-
-def simulator():
-	global millisecondCounter
-	global CPUEmpty
-	#scheduleEvent(event("CREATE_PROCESSES"))
-	
-	while checkForCompletion() == False:
-	
-		if millisecondCounter >= 1000:
-			millisecondCounter = 0
-			scheduleEvent(event("CREATE_PROCESSES"))
-			#print("fuck")
-		
-		if readyQueue and CPUEmpty:
-			scheduleEvent(event("MOVE_TO_READY"))
-		eventHandler()
-		
-		
+	#### Helper function for the eventHandler() ###
+	# Take the currentLambda and create that many processes
 def createProcesses():
+	global currentLambda
 	processGenerator(currentLambda)
 	
+	# Move the next process in the readyQueue into the currentProcess
 def selectNextProcess():
 	global currentProcess
+	global readyQueue
 	currentProcess = readyQueue.pop(0)
 	
+	# Move the currentProcess into the finishedList
 def markAsComplete():
+	global currentProcess
+	global finishedList
 	finishedList.append(currentProcess)
 	
+	# Move the currentProcess back into the readyQueue
+	# Since this is only used the Round Robin algorithm, we never have to worry about sorting these
 def returnToReady():
 	global readyQueue
 	global currentProcess
 	readyQueue.append(currentProcess)
+	### ### ### ### ### ### ### ### ### ### ### ###
+
+
+	# Adds event to the end of the event list
+def scheduleEvent(event):
+	eventQueue.append(event)
+
+	# Pops the next event from the eventQueue and process it
+def eventHandler():
+	global CPUEmpty
+	global millisecondCounter
 	
+	# Runs as long as the event is not empty
+	if eventQueue:
+		
+		currentEvent = eventQueue.pop(0)
+		
+			# Creates more processes to be added to the readyQueue
+		if currentEvent.eventType == "CREATE_PROCESSES":
+			createProcesses()
+			
+			# Only runs if the readyQueue is not empty, and then prevents anymore 'MOVE' events from being created
+		elif (currentEvent.eventType == "MOVE_TO_READY" and readyQueue):
+			selectNextProcess()
+			CPUSimulator()
+			CPUEmpty = False
+			
+			# Allows 'MOVE' events to be created, as the CPU is now finished with its current task
+		elif currentEvent.eventType == "MARK_AS_COMPLETE":
+			markAsComplete()
+			CPUEmpty = True
+			
+			# Allows 'RETURN' events to be created, as the CPU is now finished with its current task
+		elif currentEvent.eventType == "RETURN_TO_READY":
+			returnToReady()
+			CPUEmpty = True
+			
+			# lol this shouldn't ever run, if it does ¯\_(ツ)_/¯
+		else:
+			print("something went really wrong lmao")
+			return -1
+	else:
+		# If the eventQueue is empty, add a millisecond to the counter until the next 'CREATE' event is made
+		millisecondCounter = millisecondCounter + 1
+
+	# This is the main() of the simulation, which calls the eventHandler every cycle
+def simulator():
+	global millisecondCounter
+	global readyQueue
+	global CPUEmpty
+	
+		# Run until the processes completed reaches the 'TOTAL_PROCESS' global variable
+	while checkForCompletion() == False:
+	
+			# Every second schedule a 'CREATE' even
+		if millisecondCounter >= 1000:
+			millisecondCounter = 0
+			scheduleEvent(event("CREATE_PROCESSES"))
+		
+			# Move the next process into the CPU if the processQueue is not empty and CPU is
+		if readyQueue and CPUEmpty:
+			scheduleEvent(event("MOVE_TO_READY"))
+			
+			# Handle the next event in the eventQueue
+		eventHandler()
+		
+	# The CPU handler, which determines if the current event will be complete by the current algorithm
 def CPUSimulator():
 	global elapsedMilliseconds
 	global counter
@@ -120,41 +153,39 @@ def CPUSimulator():
 	global RR
 	global currentQuantum
 	global currentProcess
-	#print("Current Process Time Remaining: " + str(currentProcess.timeRemaining))
 	
-	#printProcesses(readyQueue)
-	
-	
+		# If Round Robin is set to true, we keep track of how much work can be done, and if the event is finsihed or not
 	if RR == True:
-		# calculate how much work the CPU will do this cycle, idk: do we just use the Quantum as the number for milliseconds?
-		#I'll just use 100ms for now, because ¯\_(ツ)_/¯
+	
 		workDone = currentQuantum*1000
+			
+			# If the work done is greater than or equal to the currentProcess.timeRemaining, we can mark it as complete and schedule the event to move it to the finishedList
 		if (workDone >= currentProcess.timeRemaining):
 			elapsedMilliseconds = elapsedMilliseconds + currentProcess.timeRemaining
 			millisecondCounter = millisecondCounter + currentProcess.timeRemaining
 			currentProcess.timeRemaining = 0
 			counter = counter + 1
-			#print("Current Process Time Remaining: " + str(currentProcess.timeRemaining))
+			
 			scheduleEvent(event("MARK_AS_COMPLETE"))
+			
+			# Otherwise, we subtract the work done, and schedule the event to move it back to the readyQueue
 		else:
 			currentProcess.timeRemaining = currentProcess.timeRemaining - workDone
 			elapsedMilliseconds = elapsedMilliseconds + workDone
 			millisecondCounter = millisecondCounter + workDone
-			#print("Not done\n")
+			
 			scheduleEvent(event("RETURN_TO_READY"))
 		
-		
+		# If Round Robin is false, we allow the process to fully complete, and then mark it as finished
 	else:
 		elapsedMilliseconds = elapsedMilliseconds + (currentProcess.timeRemaining)
 		millisecondCounter = millisecondCounter + (currentProcess.timeRemaining)
 		currentProcess.timeRemaining = 0
 		counter = counter + 1
+		
 		scheduleEvent(event("MARK_AS_COMPLETE"))
 	
 	
-	
-	
-
 	
 def urand():
 	return (random.randint(0, RAND_MAX)/RAND_MAX)
@@ -172,42 +203,42 @@ def sortInsert(toBeInserted, readyQueue):
 		readyQueue.append(toBeInserted)
 	else:
 		for j in range(len(readyQueue)):
-		#print(str(j) + "\n" + str(len(readyQueue)))
 			if readyQueue[j].timeRemaining > toBeInserted.timeRemaining:
 				readyQueue.insert(j, toBeInserted)
 				break
 	
-	
-	
+	# Called when you need to create new processes to be added to the readyQueue
 def processGenerator(numberOfProcesses):
 	global sort
 	global currentProcess
+	global readyQueue
+	
+	# Creates the number of processes determined by the current Lambda, with the timeRemaining set a random range always greater than zero
 	for i in range(0, numberOfProcesses):
 		nextTotalTime = int(1000*genexp(1/0.06))
-		
-		
 		if nextTotalTime == 0:
 			nextTotalTime = 1
 			
 		newProcess = process(nextTotalTime)
 			
+			# If we're keeping the readyQueue sorted, we just sort them during the insert so we don't have to worry about it later
 		if sort == True:
 		
 			sortInsert(newProcess, readyQueue)
 			
+				# We also check if the currentProcesses.timeRemaining is larger than the first process in the sorted readyQueue, and swap if necessary
 			if currentProcess.timeRemaining > readyQueue[0].timeRemaining:
 				temp = currentProcess
 				currentProcess = readyQueue[0].pop()
 				sortInsert(temp, readyQueue)
+			# Otherwise, we don't need to sort, so the event can be added to the end of the readyQueue
 		else:
 			readyQueue.append(process(nextTotalTime))
 	
+	# This is the main check to see if we're done processing yet
 def checkForCompletion():
 	global counter
 	global TOTAL_PROCESSES
-	
-	#print("\nCurrent Counter: " + str(counter))
-	#print("Total: " + str(TOTAL_PROCESSES))
 	
 	if counter < TOTAL_PROCESSES:
 		return False
@@ -215,23 +246,16 @@ def checkForCompletion():
 		return True
 
 
-def sortByTimeRemaning(readyQueue):
-	length = len(readyQueue)
-	
-	for i in range(length):
-		for j in range(0, length-i-1):
-			if readyQueue[j].timeRemaining > readyQueue[j+1].timeRemaining:
-				readyQueue[j].timeRemaining, readyQueue[j+1].timeRemaining = readyQueue[j+1].timeRemaining, readyQueue[j].timeRemaining
-
-
+	# Resets the simulator
 def resetSimulator():
 	global eventQueue
 	global readyQueue
 	global elapsedMilliseconds
+	global millisecondCounter
 	global counter
 	global StartTime
-	global millisecondCounter
 	global CPUEmpty
+	
 	eventQueue.clear()
 	readyQueue.clear()
 	elapsedMilliseconds = 0
@@ -240,24 +264,57 @@ def resetSimulator():
 	StartTime = datetime.datetime.now()
 	CPUEmpty = True
 	
-
-#print(str(int(1000*genexp(1/0.06))) + " milliseconds")
-
-
-
-
-
-
-for i in range(1, 31):
-	#print(i)
-	currentLambda = i
-	simulator()
-	print("\nCurrent Lambda: " + str(i))
-	print("Start Time: " + str(StartTime))
-	print("Total Milliseconds: " + str(elapsedMilliseconds))
-	print("Final Length of readyQueue: " + str(len(readyQueue)))
-	#printProcesses(readyQueue)
-	resetSimulator()
 	
+	# Sets the currentRunType
+def setRunType(selectedRunType):
 	
+	if selectedRunType == "FCFS":
+		sort = False
+		RR = False
+		
+	elif selectedRunType == "SRTF":
+		sort = True
+		RR = False
+		
+	elif selectedRunType == "RR02":
+		sort = False
+		RR = True
+		currentQuantum = 0.2
+		
+	elif selectedRunType == "RR001":
+		sort = False
+		RR = True
+		currentQuantum = 0.01
+		
+	else:
+		print("something is broken lol")
 
+	# Runs the simulator all 30 times
+def runSimulation():
+
+	for i in range(1, 31):
+
+		currentLambda = i
+		print("\nCurrent Lambda: " + str(i))
+		print("Start Time: " + str(StartTime))
+
+		simulator()
+	
+		print("Total Milliseconds: " + str(elapsedMilliseconds))
+		print("Final Length of readyQueue: " + str(len(readyQueue)))
+	
+		resetSimulator()
+
+
+
+setRunType("FCFS")
+runSimulation()
+
+setRunType("SRTF")
+runSimulation()
+
+setRunType("RR02")
+runSimulation()
+
+setRunType("RR001")
+runSimulation()
